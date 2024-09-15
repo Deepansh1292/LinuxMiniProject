5ebf6e04823965bb718cc3cb108c4be873145c52
Deepansh <deepanshsharma5mar2004@gmail.com>
2024-09-15 12:43:17 +0530

    Linus Mini Project

--- mySolution.sh
+++ mySolution.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-wget https://exit-zero-academy.github.io/DevOpsTheHardWayAssets/linux_project/secretGenerator.tar.gz
+
+# Download and extract the file
+wget https://exit-zero-academy.github.io/DevOpsTheHardWayAssets/linux_project/s>
 tar -xzvf secretGenerator.tar.gz
-cd src
+
+# Navigate into the src directory
+cd secretGenerator/src
+
+# Remove malicious files if they exist
 rm -rf maliciousFiles
-mkdir secretDir
+
+# Create necessary directory and file with appropriate permissions
+mkdir -p secretDir
 touch .secret
 chmod 600 .secret
+
+# Navigate back to the parent directory
 cd ..
+# Ensure generateSecret.sh is executable
 chmod +x generateSecret.sh
+
+# Execute the generateSecret.sh script
 ./generateSecret.sh
+
